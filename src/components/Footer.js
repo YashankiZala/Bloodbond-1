13e86da5c046c8558a5c65d753376c857763bf1a
VHKoisa <126274914+VHKoisa@users.noreply.github.com>
2024-11-15 11:51:51 +0530

    class -> className

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,47 +6,47 @@ const Footer = () => {
   return (
     <div className="mt-5 h-auto md:h-72 bg-[url('https://t3.ftcdn.net/jpg/02/62/65/20/360_F_262652000_Vm0XlmMMDuEGgJ2H1sMXQcxa2vU34RuQ.jpg')] md:bg-[url('https://wallpaperaccess.com/full/3272197.jpg')] bg-cover">
 
-      <div class="flex flex-col md:flex-row justify-evenly items-center">
+      <div className="flex flex-col md:flex-row justify-evenly items-center">
         {/* Column 1 */}
-        <div class="flex flex-col items-center">
-        <img class="object-cover w-auto h-36" src={logo} alt="BloodBond_logo"/>
+        <div className="flex flex-col items-center">
+        <img className="object-cover w-auto h-36" src={logo} alt="BloodBond_logo"/>
         {/* Social icons div */}
-        <div class="flex gap-4 text-red-950 md:text-2xl">
-          <i class="fa-brands fa-x-twitter cursor-pointer hover:text-black"></i>
-          <i class="fa-brands fa-facebook cursor-pointer hover:text-black"></i>
-          <i class="fa-brands fa-instagram cursor-pointer hover:text-black"></i>
-          <i class="fa-brands fa-linkedin-in cursor-pointer hover:text-black"></i>
+        <div className="flex gap-4 text-red-950 md:text-2xl">
+          <i className="fa-brands fa-x-twitter cursor-pointer hover:text-black"></i>
+          <i className="fa-brands fa-facebook cursor-pointer hover:text-black"></i>
+          <i className="fa-brands fa-instagram cursor-pointer hover:text-black"></i>
+          <i className="fa-brands fa-linkedin-in cursor-pointer hover:text-black"></i>
         </div>
         </div>
 
        {/* Column 2 */}
-       <div class="flex flex-col gap-4 my-6 -ml-3 md:-ml-0 md:my-0">
+       <div className="flex flex-col gap-4 my-6 -ml-3 md:-ml-0 md:my-0">
         <div>
-          <a class="text-red-950 ubuntu-bold" href="">Contact</a>
-          <div class="h-[2px] w-16 bg-red-900"></div>
+          <a className="text-red-950 ubuntu-bold" href="">Contact</a>
+          <div className="h-[2px] w-16 bg-red-900"></div>
         </div>
-        <div class="flex flex-col gap-2">
-          <a class="text-red-900 ubuntu-medium" href="">Call</a>
-          <a class="text-red-900 ubuntu-medium" href="">Loaction</a>
+        <div className="flex flex-col gap-2">
+          <a className="text-red-900 ubuntu-medium" href="">Call</a>
+          <a className="text-red-900 ubuntu-medium" href="">Loaction</a>
         </div>
        </div>
 
        {/* Column 3 */}
-       <div class="flex flex-col gap-4">
+       <div className="flex flex-col gap-4">
         <div>
-          <a class="text-red-950 ubuntu-bold" href="">Navigation</a>
-          <div class="h-[2px] w-16 bg-red-900"></div>
+          <a className="text-red-950 ubuntu-bold" href="">Navigation</a>
+          <div className="h-[2px] w-16 bg-red-900"></div>
         </div>
-        <div class="flex flex-col gap-2">
-          <a class="text-red-900 ubuntu-medium" href="">Home</a>
-          <a class="text-red-900 ubuntu-medium" href="">About Us</a>
+        <div className="flex flex-col gap-2">
+          <a className="text-red-900 ubuntu-medium" href="">Home</a>
+          <a className="text-red-900 ubuntu-medium" href="">About Us</a>
         </div>
        </div>
        {/* Column 3 ends */}
 
        {/* column 4 */}
-        <img class="hidden md:block mt-5" src={heartbeat3} alt=""/>
-        <img class="md:hidden" src={heart} alt=""/>
+        <img className="hidden md:block mt-5" src={heartbeat3} alt=""/>
+        <img className="md:hidden" src={heart} alt=""/>
        {/* Column 4 ends here */}
       </div>
     </div>
--- src/components/WeHealLivesSection.js
+++ src/components/WeHealLivesSection.js
@@ -6,23 +6,23 @@ import weEncourage from "../Assets/weEncourage.svg";
 const WeHealLivesSection = () => {
   return (
     <div className="bg-white h-auto md:h-72">
-      <div class="flex flex-col items-center mt-12">
-        <h1 class="ubuntu-medium text-2xl md:text-5xl">We <span class="text-red-900">Heal</span> Lives</h1>
-        <div class="flex flex-col gap-14 items-center md:flex-row md:justify-evenly w-full h-full mt-9">
+      <div className="flex flex-col items-center mt-12">
+        <h1 className="ubuntu-medium text-2xl md:text-5xl">We <span className="text-red-900">Heal</span> Lives</h1>
+        <div className="flex flex-col gap-14 items-center md:flex-row md:justify-evenly w-full h-full mt-9">
           {/* Image 1 */}
-          <div class="flex gap-4 items-center">
+          <div className="flex gap-4 items-center">
             <img src={weDonate} alt=""/>
-            <h1 class="text-base text-red-900 font-bold">We Donate</h1>
+            <h1 className="text-base text-red-900 font-bold">We Donate</h1>
           </div>
           {/* Image 2 */}
-          <div class="flex gap-4 items-center">
+          <div className="flex gap-4 items-center">
             <img src={weConnect} alt=""/>
-            <h1 class="text-base text-red-900 font-bold">We Connect</h1>
+            <h1 className="text-base text-red-900 font-bold">We Connect</h1>
           </div>
           {/* Image 3 */}
-          <div class="flex gap-4 items-center">
+          <div className="flex gap-4 items-center">
             <img src={weEncourage} alt=""/>
-            <h1 class="text-base text-red-900 font-bold">We Encourage</h1>
+            <h1 className="text-base text-red-900 font-bold">We Encourage</h1>
           </div>
         </div>
       </div>
